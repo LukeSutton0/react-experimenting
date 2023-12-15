8c8176e6b42915c7427ac28deb411a9d29759cd3
Luke <luke.sutton.work@gmail.com>
2023-12-15 13:40:45 +0000

    Added card handler logic to root
    Added card handler logic,
    setCardList
    Have to fetch cards rather than import due to vite and tsx

--- src/components/CardHandler.tsx
+++ src/components/CardHandler.tsx
@@ -1,14 +1,46 @@
 import React from 'react'
+import { Link, To } from 'react-router-dom';
+import styles from './Cards.module.css';
+
 
 interface CardPropsInterface {
-    isDarkTheme: boolean;
-  }
-
-
-function CardHandler({isDarkTheme}:CardPropsInterface) {
-  return (
-    <div>CardHandler goes here</div>
-  )
+  isDarkTheme: boolean;
+  cardList: {
+    cardName: string;
+    cardProperties: {
+      linkTo: string;
+      img: {
+        url: string;
+        altText: string;
+      };
+      title: string;
+      docsLink: string;
+    };
+  }[];
 }
+
+const CardHandler: React.FC<CardPropsInterface> = ({ isDarkTheme, cardList }) => {
+  console.log(cardList);
+  return (
+    <div>
+      
+      {cardList.map((cardName, index) => (
+        <div key={index} className={styles.cardBody}>
+          <Link to={cardName.cardProperties.linkTo} className={styles.cardImage}>
+            <img src={`src/assets/cardspecific/${cardName.cardProperties.img.url}`} alt={cardName.cardProperties.img.altText} />
+          </Link>
+          <div className={styles.cardTitle}>
+            <Link to={cardName.cardProperties.linkTo}>
+              <p>{cardName.cardProperties.title}</p>
+            </Link>
+          </div>
+          <a className={styles.cardDocs} target="_blank" href={cardName.cardProperties.docsLink}>
+            <img src="src/assets/icons8-view-48-black.png" alt="View Docs" />
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default CardHandler
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -4,14 +4,29 @@ import MainHeader from "../components/Main/MainHeader/MainHeader";
 import CardHandler from "../components/CardHandler.tsx"
 import { useEffect, useState } from "react";
 
-const cardListData = "src/assets/cardList.json"
 
 export default function Root() {
 
     const [isDarkTheme, setIsDarkTheme] = useState<boolean>(true);
+    const [cardList, setCardList] = useState([]);
+
     const handleThemeToggle = () => {
       setIsDarkTheme((prevTheme) => !prevTheme);
     };
+
+    const fetchCards = async () => {
+      try {
+        const response = await fetch("/src/assets/cardList.json");
+        const data = await response.json();
+        setCardList(data);
+      } catch (error) {
+        console.error("Error fetching cards:", error);
+      }
+    };
+    
+    useEffect(() => {
+      fetchCards();
+    }, []); //on mount
     return (
       <>
         <div>
@@ -19,8 +34,7 @@ export default function Root() {
         </div>
         <div className={styles.centreNav}>
           <nav className={styles.navbar}>
-            {cardListData.cards.map((card, index) => (
-            <CardHandler key={index} isDarkTheme={isDarkTheme} card={card} />
+            <CardHandler isDarkTheme={isDarkTheme} cardList={cardList} />
           </nav>
         </div>  
         <Outlet/>
