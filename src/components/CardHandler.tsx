5a9fb0ae0f2268d2bdd05ed4e0ac9c600cebe5d1
Luke <80263461+LukeSutton0@users.noreply.github.com>
2023-12-19 22:22:51 +0000

    Finished Card Changes
    Finished use state changes for card body colour,
    Now to work on actual main body and merge branch

--- src/components/CardHandler.tsx
+++ src/components/CardHandler.tsx
@@ -38,20 +38,17 @@ const CardHandler = ({isDarkTheme, cardList}:CardListProps) => {
   
 
   useEffect(() => {
-    console.log("Dark theme button card",isDarkTheme)
     // Update the image source based on dark mode
     const updatedImage = isDarkTheme ? 'src/assets/icons8-view-48-black.png': 'src/assets/icons8-view-48.png';
     const updatedCardBodyColour = isDarkTheme? backgroundColours.black : backgroundColours.white; 
     setViewDocsImage(updatedImage);
     setCardBodyColour(updatedCardBodyColour);
-    // document.cardBody.style.background = cardBodyColour;
-
   }, [isDarkTheme]);
 
   return (
     <>
       {cardList.cards.map((card, index) => (
-        <div key={index} className={styles.cardBody}>
+        <div key={index} className={styles.cardBody} style={{backgroundColor: cardBodyColour}}>
           <Link to={card.cardProperties.linkTo} className={styles.cardImage}>
             <img src={`src/assets/cardspecific/${card.cardProperties.img.url}`} alt={card.cardProperties.img.altText} />
           </Link>
